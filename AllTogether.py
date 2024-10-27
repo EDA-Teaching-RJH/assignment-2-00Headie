c492eacf4e89d40e76f04b1bf765209670e4918b
00Headie <va221@kent.ac.uk>
2024-10-27 18:33:48 +0000

    Not as bad as expected... good progress

--- AllTogether.py
+++ AllTogether.py
@@ -1,3 +1,11 @@
 import random
 numbers = [random.randint(1,100) for _ in range(10)]
 print("Original list:", numbers)
+
+i = 0
+while i < len(numbers):
+    if numbers[i] % 2 == 0:
+        numbers.pop(i)
+    else:
+        i += 1
+print("List of odd numbers:", numbers)
